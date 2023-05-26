07caf3bdebfd4f7205dc41d68dcc2e6ff7b96361
lulek1410 <>
2023-05-26 15:02:22 +0200

    remove redundant import

--- src/components/tests/Header.test.js
+++ src/components/tests/Header.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import {render, screen } from "@testing-library/react";
 import Header from "../Header";
 import { MemoryRouter } from "react-router-dom";
 
