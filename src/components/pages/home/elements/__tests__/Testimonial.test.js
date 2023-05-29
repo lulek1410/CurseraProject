7a00d243742835cb7d885c1fe2895a4ee6cf8fbd
lulek1410 <>
2023-05-29 17:30:15 +0200

    Add Testimonial tests

---
+++ src/components/pages/home/elements/__tests__/Testimonial.test.js
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import Testimonial from "../Testimonial";
+
+describe("Testimonial", () => {
+	const userName = "Michael Jameson";
+	const text = "Great place. Recommend.";
+
+	const renderComponent = () => {
+		render(<Testimonial userName={userName} text={text} />);
+	};
+
+	test("a", () => {
+		renderComponent();
+
+		const starsImage = screen.getByRole("img");
+		expect(starsImage).toBeInTheDocument();
+		expect(starsImage).toHaveAttribute("src", "stars.png");
+
+		const name = screen.getByRole("heading", { name: userName });
+		expect(name).toBeInTheDocument();
+
+		const review = screen.getByText(text);
+		expect(review).toBeInTheDocument();
+	});
+});
