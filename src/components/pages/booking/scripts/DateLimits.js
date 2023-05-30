8fbc343a05b127be8ae57ccdd83385f554b8c6c6
lulek1410 <>
2023-05-31 00:51:02 +0200

    Fix date formating

--- src/components/pages/booking/scripts/DateLimits.js
+++ src/components/pages/booking/scripts/DateLimits.js
@@ -1,21 +1,21 @@
+const formatDate = (date) => {
+	const monthNumber = date.getMonth();
+	const month = monthNumber < 9 ? `0${monthNumber + 1}` : monthNumber + 1;
+	const dayNumber = date.getDate();
+	const day = dayNumber < 10 ? `0${dayNumber}` : dayNumber;
+	return `${date.getFullYear()}-${month}-${day}`;
+};
+
 const getMinDate = () => {
 	const minDate = new Date();
 	minDate.setDate(minDate.getDate() + 1);
-	const month =
-		minDate.getMonth() < 9
-			? `0${minDate.getMonth() + 1}`
-			: minDate.getMonth() + 1;
-	return `${minDate.getFullYear()}-${month}-${minDate.getDate()}`;
+	return formatDate(minDate);
 };
 
 const getMaxDate = () => {
-	const minDate = new Date();
-	minDate.setDate(minDate.getDate() + 70);
-	const month =
-		minDate.getMonth() < 9
-			? `0${minDate.getMonth() + 1}`
-			: minDate.getMonth() + 1;
-	return `${minDate.getFullYear()}-${month}-${minDate.getDate()}`;
+	const maxDate = new Date();
+	maxDate.setDate(maxDate.getDate() + 70);
+	return formatDate(maxDate);
 };
 
 export const minDate = getMinDate();
